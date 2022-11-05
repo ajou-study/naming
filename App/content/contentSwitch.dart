2f3442915d6cf963a85bb8c4211277812e60a56f
n_heum <nakisara01@ajou.ac.kr>
2022-11-05 22:04:58 +0900

    heum

---
+++ App/content/contentSwitch.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class contentSwitch extends StatelessWidget{
+  contentSwitch({required this. switchOnOff});
+
+  bool switchOnOff;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
